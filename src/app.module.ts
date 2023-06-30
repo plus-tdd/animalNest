72d5f125247311ba387019e81e03e2491d72214f
kjjdsa <kjjdsa@gmail.com>
2023-06-30 22:21:37 +0900

    set env convention

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { CounselingEntity } from './module/counseling/data/counseling.entity';
 import { CounselingService } from './module/counseling/domain/counseling.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import * as process from "process";
 
 // Module 설명 : express에서는 router위주의 설계였다면, nest에서는 module위주의 설계를 한다
 // 기능별로 module을 만들어서 여기에 다 넣어줄거임 - nest가 module간의 연결된걸 파악해서 한번에 실행해줌
@@ -30,10 +31,10 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
+      port: Number(process.env.PORT),
+      username: process.env.USER,
       password: process.env.DBPW,
-      database: 'animalnest',
+      database: process.env.PW,
       synchronize: true,
       dropSchema: true,
       entities: [CounselingEntity, Pet, User, Payment],
