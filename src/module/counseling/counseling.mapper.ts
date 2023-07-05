28c51e21a9c04f62843f08e8ecaa6c34ff3888e3
unknown <codeing999@gmail.com>
2023-07-06 02:18:18 +0900

    매퍼 변경

--- src/module/counseling/counseling.mapper.ts
+++ src/module/counseling/counseling.mapper.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { CreateCounselingDto } from './api/counseling.dto';
 import { CounselingEntity } from './data/counseling.entity';
+import { Counseling } from './domain/counseling.model';
 
 @Injectable()
 export class CounselingMapper {
-  mapToEntity(dto: CreateCounselingDto): CounselingEntity {
-    return plainToClass(CounselingEntity, dto);
+  mapToEntity(counseling: Counseling): CounselingEntity {
+    return plainToClass(CounselingEntity, counseling);
   }
 
-  mapToDto(entity: CounselingEntity): CreateCounselingDto {
-    return plainToClass(CreateCounselingDto, entity);
+  mapToDto(counseling: Counseling): CreateCounselingDto {
+    return plainToClass(CreateCounselingDto, counseling);
   }
 }
