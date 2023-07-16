f1a07a2a8e957b56bcca6fc76c5f0ef8b73b4aae
codeing999 <codeing999@gmail.com>
2023-07-17 05:11:03 +0900

    도커 수정

--- src/module/doctor/doctor.seeder.ts
+++ src/module/doctor/doctor.seeder.ts
@@ -1,23 +1,26 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { DataFactory, FactoryValue, Seeder } from "nestjs-seeder";
-import { DoctorRepositoryImpl } from "./data/doctor.db";
-import { DoctorEntity } from "./data/doctor.entity";
-import { Doctor } from "./domain/doctor.model";
-import { DOCTOR_REPOSITORY, DoctorRepository } from "./domain/docter.repository";
+import { Inject, Injectable } from '@nestjs/common';
+import { DataFactory, FactoryValue, Seeder } from 'nestjs-seeder';
+import { DoctorRepositoryImpl } from './data/doctor.db';
+import { DoctorEntity } from './data/doctor.entity';
+import { Doctor } from './domain/doctor.model';
+import {
+  DOCTOR_REPOSITORY,
+  DoctorRepository,
+} from './domain/docter.repository';
 
 @Injectable()
 export class DoctorSeeder implements Seeder {
-    constructor(
-      @Inject(DOCTOR_REPOSITORY)
-      private readonly doctorRepository : DoctorRepository
-    ) {}
+  constructor(
+    @Inject(DOCTOR_REPOSITORY)
+    private readonly doctorRepository: DoctorRepository,
+  ) {}
 
-    seed(): Promise<any> {
-        const doctors = DataFactory.createForClass(DoctorEntity).generate(10);
+  seed(): Promise<any> {
+    const doctors = DataFactory.createForClass(DoctorEntity).generate(10);
 
-        return this.doctorRepository.createMany(doctors);
-    }
-    drop(): Promise<any> {
-        return this.doctorRepository.deleteAll();
-    }
+    return this.doctorRepository.createMany(doctors);
+  }
+  drop(): Promise<any> {
+    return this.doctorRepository.deleteAll();
+  }
 }
