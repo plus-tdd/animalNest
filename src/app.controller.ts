45d794a5abdf44146060228e190cb009751aca42
kjjdsa <kjjdsa@gmail.com>
2023-07-20 01:16:56 +0900

    클라우드 워치 CICD 적용 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import Logger from "./logger";
 
 @Controller() // prefix 엔드포인트
 export class AppController {
@@ -7,6 +8,8 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const logger = new Logger('application.app.controller')
+    logger.info('안녕하세요', '대문입니다.')
     return this.appService.getHello();
   }
 }
