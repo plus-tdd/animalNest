0957953b6d3bc86a8ebf81b5926a429f55285152
kjjdsa <kjjdsa@gmail.com>
2023-07-26 23:06:04 +0900

    회원가입 try catch + 에러 로깅 표본 작성

--- src/module/user/api/user.controller.ts
+++ src/module/user/api/user.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from '../domain/user.service';
 import { SignUpDto } from './user.dto';
+import Logger from "../../../logger";
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  private logger : Logger;
+  constructor(private readonly userService: UserService) {
+    this.logger = new Logger('user.controller');
+  }
 
   @Post('signup')
   signUp(@Body() signUpDto: SignUpDto) {
-    return this.userService.signUp(signUpDto);
+    try {
+      this.logger.info(`회원가입 성공, ${signUpDto.account} 님 환영합니다`)
+      return this.userService.signUp(signUpDto);
+    } catch(e) {
+      this.logger.error( e,`회원가입 실패, ${signUpDto.account} 님 미안합니다`)
+    }
   }
 }
